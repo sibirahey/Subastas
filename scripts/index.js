6cd9c71b597f87e4beeedaf0335d9a1bca7a8424
msusano1984 <msusano@116037-LAP.local>
2017-01-23 11:58:58 -0600

    Calendar

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,14 @@
 
+var calendars = {};
+
 $( document ).ready(function() {
     $( ".mainBody" ).load( "views/index.html", function() {
   		console.log( "Load was performed." );
+
+	    calendars.clndr1 = $('.cal1').clndr();
+	
 	});
+	
+
 
 });
