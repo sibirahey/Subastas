d287d1675664a9dca042d21a2ebd76ab77ee1c2f
Beezel Palomar <beezelpalomar@gmail.com>
2017-01-23 11:05:56 -0600

    Ult Cambios
    ult cambios JMP

---
+++ Subastas/scripts/footer.js
@@ -0,0 +1,6 @@
+
+$( document ).ready(function() {
+    $( ".mainFooter" ).load( "footer.html", function() {
+  		console.log( "Load was performed." );
+	});
+});
---
+++ Subastas/scripts/header.js
@@ -0,0 +1,6 @@
+
+$( document ).ready(function() {
+    $( ".mainHeader" ).load( "header.html", function() {
+  		console.log( "Load was performed." );
+	});
+});
