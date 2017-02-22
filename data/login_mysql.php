56b84523b5757772514e87df09387daef73c8fb2
msusano1984 <msusano@116037-LAP.local>
2017-02-22 02:14:12 -0600

    no message

--- data/login_mysql.php
+++ data/login_mysql.php
@@ -3,7 +3,9 @@
  * Provee las constantes para conectarse a la base de datos
  * Mysql.
  */
+
 define("NOMBRE_HOST", "localhost");// Nombre del host
 define("BASE_DE_DATOS", "subastas_db"); // Nombre de la base de modelos
 define("USUARIO", "subastas_usr"); // Nombre del usuario
 define("CONTRASENA", "sub4st4s_m41n*"); // Constraseña
+
