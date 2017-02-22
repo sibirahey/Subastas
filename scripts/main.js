120c33480859f66b20db728c06a3ed785c46b3bc
smeza <smezac@tvazteca.com.mx>
2017-02-21 20:21:59 -0600

    ok

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,45 @@
+function CargaDatos(){
+
+	$.ajax({
+		dataType : "json",
+		url : "data/subastas.json",
+		data :"data",
+		success: function(data){
+
+			$.each(data["datos"],function(i,item){
+
+				var renglon = "<div class='rows_Ofertas'>";
+				renglon += "<div>"+ item.empresa+"</div>";
+				renglon += "<div>"+ item.estatus+"</div>";
+				renglon += "<div>"+ item.fechaInicio+"</div>";
+				renglon += "<div>"+ item.fechaFin+"</div>";
+				renglon += '<div><button type="button" attr-id="'+item.id+'" onclick="VerSubasta(this);">Ver Subasta</button></div>';				
+				renglon += "</div>";
+
+				$("#grdResultados").append(renglon);
+
+			});
+
+
+		}
+	});
+
+
+}
+function VerSubasta(o){
+	alert($(o).attr("attr-id"));
+
+
+}
 $(document).ready(function(){
 
   cargaHTML(".mainBody", "views/main.html","", function() {
-  		console.log( "carga main");
+  		
+  		CargaDatos();
+
+
+
+
   	});
 
   });
