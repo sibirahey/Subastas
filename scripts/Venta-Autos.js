b72e015b9dcf7833e825b20635707c19965a4eba
Beezel Palomar <beezelpalomar@gmail.com>
2017-04-21 11:46:04 -0500

    IMplementacionde mATERIALIZE.JS Venta Autos y admin subastas

--- scripts/Venta-Autos.js
+++ scripts/Venta-Autos.js
@@ -13,7 +13,8 @@ $("#cmbMarcas").change(function(){
 }
 
 function muestraGaleria(idx) {
-
+	debugger;
+	
 	var dialog = $("#gallery" + idx).dialog({
 		autoOpen : false,
 		height : 400,
@@ -25,7 +26,7 @@ function muestraGaleria(idx) {
 
 	$("#gallery" + idx).addClass('muestraGaleria');
 
-	dialog.dialog("open");
+	//dialog.dialog("open");
 }
 
 function seleccionaImagen(obj) {
@@ -55,21 +56,27 @@ function cargaVehiculos() {
 			$.each(data["vehiculos"], function(i, item) {
 				// if (FiltrarAutos(item)) {
 					var renglon = "<div value class='rows'>";
-					renglon += '<div><img alt="' + item.vehiculo + '" src="' + item.foto + '"  /></div>';
+					renglon += '<div><img alt="' + item.vehiculo + '" src="' + item.foto + '" onerror=\'imgError(this)\'; /></div>';
 					renglon += "<div>" + item.descripcion + "</div>";					renglon += "<div>" + item.estado + "</div>";
 					renglon += "<div>" + item.kms + "</div>";
-					renglon += "<div><img src='images/icoCamara.svg' onclick='muestraGaleria(" + item.idVehiculo + ");' /></div>";
+					//renglon += "<div><button class='btn waves-effect light-blue lighten-1'  onclick='muestraGaleria(" + item.idVehiculo + ");'><i class='material-icons'>photo_camera</i></button></div>";
+					renglon += "<div><button class='btn waves-effect waves-light light-blue lighten-1'  data-target='gallery"+ item.idVehiculo +"'><i class='material-icons'>photo_camera</i></button></div>";
 					renglon += "<div>" + item.precio + "</div>";
-					renglon += "<div><button>AGREGAR</button></div>";
-					renglon += "<div style='display:none;' id='gallery" + item.idVehiculo + "'>";
+					renglon += "<div class='center-btn'><button class='btn waves-effect waves-light light-blue lighten-1'><i class='material-icons'>add</i></button></div>";
+					//renglon += "<div style='display:none;' id='gallery" + item.idVehiculo + "'>";
+					renglon += "<div id='gallery"+ item.idVehiculo +"'  class='modal modal-fixed-footer'>";
+				    renglon += "	<div class='modal-content'>";
+			        renglon += "	<h4>Modal Header</h4>";
 					$.each(item.imagenes, function(j, item2) {
-						renglon += "<div class='galleryunselected'><img src='fotos/" + item2 + "' onclick='seleccionaImagen(this);' onerror='images/missingImage.jpg' /></div>"
+						renglon += "<div class='galleryunselected'><img src='fotos/" + item2 + "' onclick='seleccionaImagen(this);' onerror='imgError(this)';/></div>"
 					});
+					renglon += "	</div>";
+					renglon += " </div>";
 					renglon += "</div>";
-
 					renglon += "</div>";
 					
 					$("#grdVehiculos").append(renglon);
+					$('.modal').modal();
 
 				//º }
 					
@@ -79,6 +86,12 @@ function cargaVehiculos() {
 	});
 
 };
+
+function imgError(image) {
+    image.onerror = "";
+    $(image).attr('src','images/imagenNoDisponible.svg');
+    return true;
+}
 
 function FiltrarAutos(item) {
 	 var precioMax = $('#cmbPrecio option:selected').attr('max');
@@ -162,4 +175,5 @@ $(document).ready(function() {
 		$('.dateTimeHeader').hide();
 	
 	});
+	
 });
