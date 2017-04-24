d1ffb8d7d0f2ddcd2385d5fe0e3052e0721008b7
Beezel Palomar <beezelpalomar@gmail.com>
2017-04-24 10:55:05 -0500

    Linea130 faltaba una llave de cierre

--- scripts/entities.js
+++ scripts/entities.js
@@ -127,7 +127,7 @@ function Home(banner970x90_01, infoSeguridad, banner300x600){
 	this.banner970x90_01 = banner970x90_01;
 	this.infoSeguridad = infoSeguridad;
 	this.banner300x600 = banner300x600;
-
+}
 function busquedaAuto(descripcion,estadoId,marcaId,modeloId,anio,precioIni,precioFin,kmIni,kmFin){
 	this.descripcion = descripcion;
 	this.estadoId = estadoId;
