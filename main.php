e202dd48bacb13fb77b47fa80f24109e71987e2a
MIGUEL ANGEL SUSANO GILES <miguel.susano@gmail.com>
2017-06-19 01:33:53 -0500

    mis cotizaciones

--- main.php
+++ main.php
@@ -77,6 +77,7 @@
 		<script type="text/javascript" src="scripts/MisAutos.js<?php echo "?" . rand(0, 9999999); ?>"></script>
 		<script type="text/javascript" src="scripts/modulos/admin/resultadossubastas.js<?php echo "?" . rand(0, 9999999); ?>"></script>
 		<script type="text/javascript" src="scripts/jcarousel.js<?php echo "?" . rand(0, 9999999); ?>"></script>
+		<script type="text/javascript" src="scripts/MisCotizaciones.js<?php echo "?" . rand(0, 9999999); ?>"></script>
 	</body>
 	<!-- contenedor del cuerpo principal del sitio -->
 </html>
